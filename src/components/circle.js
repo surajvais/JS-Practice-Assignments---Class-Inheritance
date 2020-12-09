4ec10ea6fa7eb779ea544e6cc368a5a2e96d6414
surajvais <37078537+surajvais@users.noreply.github.com>
2020-12-09 12:55:59 +0530

    dd

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,6 +1,16 @@
-// import using require
+let shape = require("./shape.js");
 
 // declare class
 
-
 // export class using module.exports
+
+class circle extends shape {
+  constructor() {
+    super();
+    this.calculateArea = () => {
+      console.log("circle calc area");
+    };
+  }
+}
+
+module.exports.circle = circle;
--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,20 @@
 // declare class
 
 // export class using module.exports
+
+class shape {
+  constructor() {
+    super();
+    this.drawShape = function () {
+      console.log("draw shape");
+    };
+
+    this.calculateArea = () => {
+      console.log("shape calc area");
+    };
+
+    this.color = "blue";
+  }
+}
+
+module.exports.shape = shape;
